809d6eaa3f4c25bc64b9a7808285a71a840886d9
Nacho Ortega <74309113+pikanachi@users.noreply.github.com>
2022-10-24 20:24:25 +0200

    Tests implemented

--- src/main/kotlin/es/unizar/webeng/lab3/Application.kt
+++ src/main/kotlin/es/unizar/webeng/lab3/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Main
+class Application
 
 fun main(vararg args: String) {
-    runApplication<Main>(*args)
+    runApplication<Application>(*args)
 }
